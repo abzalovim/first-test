a7c2de5be5e3224bd955fb4e484b211678f8fcec
abzalovim <abzalovim@gmail.com>
2012-09-20 23:11:11 +0400

    Optimized

--- first.rb
+++ first.rb
@@ -1,11 +1,14 @@
 def counter(v_in)
-  first_symbol=v_in[0..0]
-  first_group = v_in.scan(/^#{first_symbol}+/)[0]
-  v_out = first_group.size.to_s + first_symbol
-  other_group = v_in.sub(/^#{first_symbol}+/){}
-  v_out += counter(other_group) unless other_group.empty?
+  first_symbol = v_in[0..0]
+  first_group = v_in.scan(/#{first_symbol}+/)
+  v_out = ""
+  other_group = v_in.split(/#{first_symbol}+/)
+  first_group.each_index{|i| v_out += (first_group[i].size.to_s + first_symbol) + (other_group[i+1].nil? ? "" : counter(other_group[i+1]))}
   return v_out
 end
 
 a = ARGV[0].nil? ? "1" : ARGV[0]
-puts counter(a)
+while guess = STDIN.gets
+  a = counter(a)
+  puts "#{a} size:#{a.size}"
+end
