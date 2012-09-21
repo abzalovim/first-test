c237f0e228f02f2bcfc419be920ce90b0a31eeb6
abzalovim <abzalovim@gmail.com>
2012-09-21 13:19:49 +0400

    Optimized string concatenation

--- first.rb
+++ first.rb
@@ -1,9 +1,9 @@
 def counter(v_in)
   first_symbol = v_in[0..0]
-  first_group = v_in.scan(/#{first_symbol}+/)
+  first_group = v_in.scan(/#{ first_symbol }+/)
   v_out = ""
-  other_group = v_in.split(/#{first_symbol}+/)
-  first_group.each_index{|i| v_out += (first_group[i].size.to_s + first_symbol) + (other_group[i+1].nil? ? "" : counter(other_group[i+1]))}
+  other_group = v_in.split(/#{ first_symbol }+/)
+  first_group.each_index{|i| v_out << (first_group[i].size.to_s + first_symbol) + (other_group[i+1].nil? ? "" : counter(other_group[i+1]))}
   return v_out
 end
 
